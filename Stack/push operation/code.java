7df68bffbe611719efaf12340f07261702a15e5c
GEDA SIVA RAM CHOWDARY <78960366+sivaram191@users.noreply.github.com>
2022-12-20 14:25:23 +0530

    Create code.java

---
+++ Stack/push operation/code.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class push_operation
+{
+	static void push(Stack<Integer> stack)
+	{
+		for(int i=0;i<=5;i++)
+		{
+			stack.push(i);
+		}
+		System.out.println(stack);
+	}
+	public static void main (String[] args)
+	{
+		Stack<Integer> stack= new Stack<Integer>();
+		push(stack);
+	}
+}
